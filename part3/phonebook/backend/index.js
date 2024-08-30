5391fedf8a5ea6d64f02f9652d427066d6a5a385
Fl3mish <mitch.pauwels@proton.me>
2024-08-31 00:31:45 +0100

    Finished Exercise 3.1-3.6

--- part3/phonebook/backend/index.js
+++ part3/phonebook/backend/index.js
@@ -1,4 +1,8 @@
-const data = [
+const express = require("express");
+const app = express();
+// Middleware
+app.use(express.json());
+let data = [
   {
     id: "1",
     name: "Arto Hellas",
@@ -20,3 +24,70 @@ const data = [
     number: "39-23-6423122",
   },
 ];
+
+// Routes
+// Get Info
+app.get("/api/persons/info", (req, res) => {
+  const date = new Date();
+  const entrees = data.length;
+  const message = `Phonebook has info for ${entrees} people `;
+  res.send(`<p>${message}</p>${date}`);
+});
+// GET all
+app.get("/api/persons", (req, res) => {
+  res.json(data);
+});
+
+// GET Single Person
+app.get("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(req.params);
+  const person = data.find((p) => p.id === id);
+  console.log("content: ");
+  if (!person) {
+    return res.status(400).json({
+      error: "content missing",
+    });
+  }
+  res.json(person);
+});
+
+// CREATE/POST Person
+app.post("/api/persons", (req, res) => {
+  //   const body = req.body;
+  console.log(req.body);
+  const { name, number } = req.body;
+  const isExistingPerson = data.find(
+    (p) => p.name.toLowerCase() === name.toLowerCase()
+  );
+
+  if (isExistingPerson) {
+    return res
+      .status(400)
+      .json({ error: "The name already exists in the phonebook" });
+  }
+
+  const person = {
+    id: String(Math.floor(Math.random() * 9999999)),
+    name,
+    number,
+  };
+
+  if (!person.name || !person.number) {
+    return res.status(400).json({ error: "The name or number is missing" });
+  }
+
+  data = data.concat(person);
+  res.json(person);
+});
+
+// DELETE Person
+app.delete("/api/persons/:id", (req, res) => {
+  const { id } = req.params;
+  data = data.filter((p) => p.id !== id);
+  res.status(204).end();
+});
+
+// LISTEN to open PORT
+const PORT = 3001;
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
